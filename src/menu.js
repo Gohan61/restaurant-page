064e7522a9e252681981d0694996521db14b6d73
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-02-08 14:23:45 +0100

    Add breakfast elements + change attr/append func
    Feat: added the breakfast elements to the menu
    Fix: changed the setAttribute + appendTo functions to one createField
    function so that elements can be easily set text, attributes and append
    to DOM

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,3 @@
-import { setAttributes } from "./home";
-import { appendItem } from "./home";
-
 export default function menu() {
   const contentDiv = document.querySelector("#content");
 
@@ -8,12 +5,144 @@ export default function menu() {
     contentDiv.removeChild(contentDiv.lastChild);
   }
 
-  const menuHeader = document.createElement("h1");
-  const breakfast = document.createElement("div");
-  const lunch = document.createElement("div");
-  const dinner = document.createElement("div");
+  const menuHeader = createField("h1", contentDiv, undefined, "Menu");
+  const breakfastDiv = createField(
+    "div",
+    contentDiv,
+    { class: "menuItem" },
+    undefined
+  );
+  const lunchDiv = createField(
+    "div",
+    contentDiv,
+    { class: "menuItem" },
+    undefined
+  );
+  const dinnerDiv = createField(
+    "div",
+    contentDiv,
+    { class: "menuItem" },
+    undefined
+  );
 
-  menuHeader.textContent = "Menu";
+  const breakfast = createField(
+    "h2",
+    breakfastDiv.elementName,
+    undefined,
+    "Breakfast"
+  );
+  const lunch = createField("h2", lunchDiv.elementName, undefined, "Lunch");
+  const dinner = createField("h2", dinnerDiv.elementName, undefined, "Dinner");
 
-  appendItem(menuHeader, contentDiv);
+  const breakFastDivs = {
+    eggsDiv: createField(
+      "div",
+      breakfastDiv.elementName,
+      { class: "itemDiv" },
+      undefined
+    ),
+    oatmealDiv: createField(
+      "div",
+      breakfastDiv.elementName,
+      { class: "itemDiv" },
+      undefined
+    ),
+    fruitDiv: createField(
+      "div",
+      breakfastDiv.elementName,
+      { class: "itemDiv" },
+      undefined
+    ),
+  };
+
+  const breakFastItems = {
+    scrambledEggs: createField(
+      "p",
+      breakFastDivs.eggsDiv.elementName,
+      { class: "foodItem" },
+      "Scrambled Eggs"
+    ),
+    eggsPrice: createField(
+      "p",
+      breakFastDivs.eggsDiv.elementName,
+      { class: "price" },
+      "$10"
+    ),
+
+    oatmeal: createField(
+      "p",
+      breakFastDivs.oatmealDiv.elementName,
+      { class: "foodItem" },
+      "Oatmeal"
+    ),
+    oatmealPrice: createField(
+      "p",
+      breakFastDivs.oatmealDiv.elementName,
+      { class: "price" },
+      "$5"
+    ),
+
+    fruit: createField(
+      "p",
+      breakFastDivs.fruitDiv.elementName,
+      { class: "foodItem" },
+      "Fresh fruit"
+    ),
+    fruitPrice: createField(
+      "p",
+      breakFastDivs.fruitDiv.elementName,
+      { class: "price" },
+      "$14"
+    ),
+  };
+
+  menuHeader.setText().appendElement();
+  breakfastDiv.setClass().appendElement();
+  lunchDiv.setClass().appendElement();
+  dinnerDiv.setClass().appendElement();
+
+  breakfast.setText().appendElement();
+  lunch.setText().appendElement();
+  dinner.setText().appendElement();
+
+  breakFastDivs.eggsDiv.setClass().appendElement();
+  breakFastDivs.oatmealDiv.setClass().appendElement();
+  breakFastDivs.fruitDiv.setClass().appendElement();
+
+  breakFastItems.scrambledEggs.setClass().setText().appendElement();
+  breakFastItems.oatmeal.setClass().setText().appendElement();
+  breakFastItems.fruit.setClass().setText().appendElement();
+
+  breakFastItems.eggsPrice.setClass().setText().appendElement();
+  breakFastItems.oatmealPrice.setClass().setText().appendElement();
+  breakFastItems.fruitPrice.setClass().setText().appendElement();
+}
+
+export function createField(element, appendTo, attributes, text) {
+  const elementName = document.createElement(`${element}`);
+
+  const setClass = function () {
+    elementName.setAttribute("class", attributes["class"]);
+    return this;
+  };
+
+  const setId = function () {
+    elementName.setAttribute("id", attributes["id"]);
+    return this;
+  };
+
+  const setText = function () {
+    elementName.textContent = text;
+    return this;
+  };
+
+  const appendElement = () => appendTo.append(elementName);
+
+  return {
+    elementName,
+    setClass,
+    setId,
+    setText,
+    appendElement,
+  };
 }
